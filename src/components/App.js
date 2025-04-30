558473f16753639a70dc1f61d29baa638e192ba4
Marek Bełz <belzmarek@gmail.com>
2025-04-30 16:20:11 +0000

    task 02 done

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import Task01 from './../../01/Task01';
 import Task02 from './../../02/Task02';
@@ -6,16 +7,18 @@ import Task03 from './../../03/Task03';
 import Task04 from './../../04/Task04';
 import Task05 from './../../05/Task05';
 
+import store from './../store';
+
 
 const App = () => {
     return (
-        <>
+        <Provider store={store}>
             <Task01 />
-            {/* <Task02 /> */}
+            <Task02 />
             {/* <Task03 /> */}
             {/* <Task04 /> */}
             {/* <Task05 /> */}
-        </>
+        </Provider>
     )
 }
 
