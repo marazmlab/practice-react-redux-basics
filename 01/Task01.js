80bbfebec9f36b257afd222a8346c71eeff0d905
Marek Bełz <belzmarek@gmail.com>
2025-04-30 15:59:01 +0000

    task 01 done

--- 01/Task01.js
+++ 01/Task01.js
@@ -6,27 +6,25 @@ const Task01 = () => {
     const [tagName, setTagName] = useState('??');
     const [cursorPosition, setCursorPosition] = useState('[?,?]');
 
-    const setEventTime = ({timeStamp}) => {
-        setTime( parseInt(timeStamp / 1000) );
-    }
+    const subject = new Subject();
 
-    const setEventTagName = ({target}) => {
+    subject.subscribe(({ timeStamp }) => {
+        setTime(parseInt(timeStamp / 1000));
+    });
+
+    subject.subscribe(({target}) => {
         setTagName(target.tagName);
-    }
+    })
 
-    const setEventCursorPosition = ({pageX, pageY}) => {
+    subject.subscribe(({pageX, pageY}) => {
         setCursorPosition(`[${pageX},${pageY}]`);
-    }
+    })
 
     return (
         <section>
             <h1>Task 1</h1>
             
-            <div onClick={ event => {
-                setEventTime(event);
-                setEventTagName(event);
-                setEventCursorPosition(event);
-            }}>
+            <div onClick={ (event) => subject.notify(event) }>
                 <p>
                     <strong>Kliknij wybrany element:</strong> <a>link</a>, <button>button</button>, <span>span</span>
                 </p>
